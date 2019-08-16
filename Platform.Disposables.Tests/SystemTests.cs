a3b9a78c05679e6a92539fb3f0840a3f10dadf09
Константин Дьяченко <konard@me.com>
2019-08-16 12:42:44 +0300

    Comment translated to Russian.

--- Platform.Disposables.Tests/SystemTests.cs
+++ Platform.Disposables.Tests/SystemTests.cs
@@ -1,10 +1,10 @@
-using System;
 using Xunit;
 
 namespace Platform.Disposables.Tests
 {
     /// <summary>
-    /// Contains tests for features of .NET Framework itself, that are required by current implementations.
+    /// <para>Contains tests for features of .NET Framework itself, that are required by current implementations.</para>
+    /// <para>Содержит тесты для функций самой .NET Framework, которые требуются для текущих реализаций.</para>
     /// </summary>
     public class SystemTests
     {
