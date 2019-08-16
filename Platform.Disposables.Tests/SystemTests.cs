705c434dae7d28943910f43a479189a0fc906d2a
Константин Дьяченко <konard@me.com>
2019-08-16 12:39:40 +0300

    Added tests moved from https://github.com/Konard/LinksPlatform/blob/9ab044e6355acbb9f387ea6655be7e58426631e4/Platform/Platform.Tests/SystemTests.cs

---
+++ Platform.Disposables.Tests/SystemTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace Platform.Disposables.Tests
+{
+    /// <summary>
+    /// Contains tests for features of .NET Framework itself, that are required by current implementations.
+    /// </summary>
+    public class SystemTests
+    {
+        [Fact]
+        public void UsingSupportsNullTest()
+        {
+            using (var disposable = null as IDisposable)
+            {
+                Assert.True(disposable == null);
+            }
+        }
+    }
+}
