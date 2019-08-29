b6413c96fb618337b84c0302eff385ee9de14cf4
Константин Дьяченко <konard@me.com>
2019-08-29 21:38:57 +0300

    DisposableBaseExtensions renamed to IDisposableExtensions. IDisposableExtensions.DisposeIfNotDisposed method is now uses IDisposable instead of DisposableBase for argument type.

---
+++ Platform.Disposables/IDisposableExtensions.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace Platform.Disposables
+{
+    /// <summary>
+    /// <para>Provides a set of extension methods for <see cref="IDisposable"/> objects.</para>
+    /// <para>Предоставляет набор методов расширения для объектов <see cref="IDisposable"/>.</para>
+    /// </summary>
+    public static class IDisposableExtensions
+    {
+        /// <summary>
+        /// <para>Attempts to dispose the specified object.</para>
+        /// <para>Выполняет попытку высвободить указанный объект.</para>
+        /// </summary>
+        /// <param name="disposable"><para>The object implementing the <see cref="IDisposable"/> interface.</para><para>Объект, реализующий интерфейс <see cref="IDisposable"/></para></param>
+        public static void DisposeIfNotDisposed(this IDisposable disposable)
+        {
+            if (!disposable.IsDisposed)
+            {
+                disposable.Dispose();
+            }
+        }
+    }
+}
