8288fbe769b729acdfa60a3827e681ffa97618e4
Константин Дьяченко <konard@me.com>
2019-10-24 20:22:53 +0300

    Making code shorter.

--- Platform.Disposables.Tests.DisposalOrderTest/Program.cs
+++ Platform.Disposables.Tests.DisposalOrderTest/Program.cs
@@ -20,15 +20,13 @@ namespace Platform.Disposables.Tests.DisposalOrderTest
             }
             var logPath = args[0];
             var waitForCancellation = bool.Parse(args[1]);
-            using (var consoleCancellationHandler = new ConsoleCancellation())
+            using var consoleCancellationHandler = new ConsoleCancellation();
+            var disposable1 = new Disposable(() => { File.AppendAllText(logPath, "1"); }); //-V3114
+            var disposable2 = new Disposable(() => { File.AppendAllText(logPath, "2"); }); //-V3114
+            Console.WriteLine(disposable1.IsDisposed && disposable2.IsDisposed); // Ensure objects are not optimized away
+            if (waitForCancellation)
             {
-                var disposable1 = new Disposable(() => { File.AppendAllText(logPath, "1"); }); //-V3114
-                var disposable2 = new Disposable(() => { File.AppendAllText(logPath, "2"); }); //-V3114
-                Console.WriteLine(disposable1.IsDisposed && disposable2.IsDisposed); // Ensure objects are not optimized away
-                if (waitForCancellation)
-                {
-                    consoleCancellationHandler.Wait();
-                }
+                consoleCancellationHandler.Wait();
             }
         }
     }
