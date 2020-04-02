f0b0b2573153a8a69f2b8b61789646836b4b1ff4
Konstantin Diachenko <drakonard@gmail.com>
2020-04-02 21:19:54 +0300

    Added template library's project and header file.

---
+++ cpp/Platform.Disposables/Platform.Disposables.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifndef Platform_Disposables
+#define Platform_Disposables
+
+//#include <Platform.Exceptions.h>
+//#include <Platform.Converters.h>
+//#include <Platform.Hashing.h>
+
+#include "IDisposable.h"
+#include "Disposable.h"
+#include "Disposable[T].h"
+#include "Disposable[TPrimary, TAuxiliary].h"
+#include "DisposableBase.h"
+#include "Disposal.h"
+#include "EnsureExtensions.h"
+#include "GenericObjectExtensions.h"
+#include "IDisposableExtensions.h"
+
+#endif
